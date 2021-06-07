704d14c9f9a7b4c370a229dfdfa94361addc6ae4
CPerezz <carlos@dusk.network>
2021-06-07 15:42:01 +0200

    Fix Readme.md import from lib.rs
    The feature `external_doc` has been removed and instead now another
    methodology has been enabled to include `README` or other file contents
    inside of the docs.
    
    The change has been applied to not produce errors with the latest
    nightly rustc versions (1.54.0).
    
    Resolves: #20

--- dusk-bytes/src/lib.rs
+++ dusk-bytes/src/lib.rs
@@ -4,10 +4,9 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-#![feature(external_doc)]
 #![deny(missing_docs)]
-#![doc(include = "../README.md")]
 #![no_std]
+#![doc = include_str!("../README.md")]
 
 mod errors;
 mod parse;
@@ -17,5 +16,4 @@ mod serialize;
 pub use errors::{BadLength, Error, InvalidChar};
 pub use parse::ParseHexStr;
 pub use serialize::{DeserializableSlice, Read, Serializable, Write};
-
 pub use derive_hex::{Hex, HexDebug};
